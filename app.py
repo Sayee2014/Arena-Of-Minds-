2a6838d2428fb3d58f14fdc5635794d8a0a87b3c
Sayee2014 <durvakutwal@gmail.com>
2026-08-04 21:09:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,16 @@ st.subheader("Enter the Arena. Master Your Mind.")
 
 if st.button("🚀 Begin Journey"):
     st.success("Welcome to Arena of Minds!")
+
+    st.header("📚 Dashboard")
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        st.button("📚 Learn")
+        st.button("💻 Coding")
+
+    with col2:
+        st.button("📝 Quests")
+        st.button("😌 Relax Zone")
+``
